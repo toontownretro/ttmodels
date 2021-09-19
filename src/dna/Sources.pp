ea5a6c8c356011eb88e8c93676114986bf8aabee
Bob <54867422+Bob-by@users.noreply.github.com>
2021-09-20 01:57:44 +0200

    dna Sources.pp update

--- src/dna/Sources.pp
+++ src/dna/Sources.pp
@@ -5,10 +5,14 @@
 #begin install_dna
   #define SOURCES \
     storage_tutorial.dna \
-    storage_interior.dna \
-    tutorial_street_english.dna \
-    tutorial_street_castillian.dna \
-    tutorial_street_japanese.dna
+    storage_interior.dna 
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    tutorial_street_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+	tutorial_street_english.dna
+  #endif
   #define PHASE 3.5
 #end install_dna
 
@@ -16,13 +20,17 @@
 #begin install_dna
   #define SOURCES \
     storage_TT_sz.dna \
-    storage_english.dna storage_TT_english.dna \
-    storage_japanese.dna storage_TT_japanese.dna \
     winter_storage_TT.dna winter_storage_TT_sz.dna \
-    halloween_props_storage_TT.dna halloween_props_storage_TT_sz.dna \
-    toontown_central_sz_english.dna \
-    toontown_central_sz_castillian.dna \
-    toontown_central_sz_japanese.dna
+    halloween_props_storage_TT.dna halloween_props_storage_TT_sz.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_$[LANGUAGE].dna storage_TT_$[LANGUAGE].dna \
+    toontown_central_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_english.dna storage_TT_english.dna \
+    toontown_central_sz_english.dna
+  #endif
   #define PHASE 4
 #end install_dna
 
@@ -30,10 +38,14 @@
 #begin install_dna
   #define SOURCES \
     storage_town.dna \
-    storage_TT_town.dna \
-    toontown_central_2100_english.dna toontown_central_2200_english.dna toontown_central_2300_english.dna \
-    toontown_central_2100_castillian.dna toontown_central_2200_castillian.dna toontown_central_2300_castillian.dna \
-    toontown_central_2100_japanese.dna toontown_central_2200_japanese.dna toontown_central_2300_japanese.dna
+    storage_TT_town.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    toontown_central_2100_$[LANGUAGE].dna toontown_central_2200_$[LANGUAGE].dna toontown_central_2300_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    toontown_central_2100_english.dna toontown_central_2200_english.dna toontown_central_2300_english.dna 
+  #endif
   #define PHASE 5
 #end install_dna
 
@@ -44,9 +56,14 @@
     winter_storage_estate.dna \
     halloween_props_storage_estate.dna \
     house_interior3.dna house_interior4.dna house_interior5.dna \
-    house_interior7.dna house_interior8.dna house_interior10.dna \
-    storage_estate_english.dna \
-    storage_estate_japanese.dna 
+    house_interior7.dna house_interior8.dna house_interior10.dna 
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_estate_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_estate_english.dna 
+  #endif
   #define PHASE 5.5
 #end install_dna
 
@@ -54,16 +71,19 @@
 #begin install_dna
   #define SOURCES \
     storage_DD_sz.dna storage_DD_town.dna \
-    storage_DD_english.dna \
-    storage_DD_japanese.dna \
     winter_storage_DD.dna \
-    halloween_props_storage_DD.dna \
+    halloween_props_storage_DD.dna 
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_DD_$[LANGUAGE].dna \
+    donalds_dock_1100_$[LANGUAGE].dna donalds_dock_1200_$[LANGUAGE].dna donalds_dock_1300_$[LANGUAGE].dna \
+    donalds_dock_sz_$[LANGUAGE].dna 
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_DD_english.dna \
     donalds_dock_1100_english.dna donalds_dock_1200_english.dna donalds_dock_1300_english.dna \
-    donalds_dock_1100_castillian.dna donalds_dock_1200_castillian.dna donalds_dock_1300_castillian.dna \
-    donalds_dock_1100_japanese.dna donalds_dock_1200_japanese.dna donalds_dock_1300_japanese.dna \
-    donalds_dock_sz_english.dna \
-    donalds_dock_sz_castillian.dna \
-    donalds_dock_sz_japanese.dna
+    donalds_dock_sz_english.dna 
+  #endif
   #define PHASE 6
 #end install_dna
 
@@ -71,16 +91,19 @@
 #begin install_dna
   #define SOURCES \
     storage_MM_sz.dna storage_MM_town.dna \
-    storage_MM_english.dna \
-    storage_MM_japanese.dna \
     winter_storage_MM.dna \
-    halloween_props_storage_MM.dna \
+    halloween_props_storage_MM.dna 
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_MM_$[LANGUAGE].dna \
+    minnies_melody_land_4100_$[LANGUAGE].dna minnies_melody_land_4200_$[LANGUAGE].dna minnies_melody_land_4300_$[LANGUAGE].dna \
+    minnies_melody_land_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_MM_english.dna \
     minnies_melody_land_4100_english.dna minnies_melody_land_4200_english.dna minnies_melody_land_4300_english.dna \
-    minnies_melody_land_4100_castillian.dna minnies_melody_land_4200_castillian.dna minnies_melody_land_4300_castillian.dna \
-    minnies_melody_land_4100_japanese.dna minnies_melody_land_4200_japanese.dna minnies_melody_land_4300_japanese.dna \
-    minnies_melody_land_sz_english.dna \
-    minnies_melody_land_sz_castillian.dna \
-    minnies_melody_land_sz_japanese.dna
+    minnies_melody_land_sz_english.dna
+  #endif
   #define PHASE 6
 #end install_dna
 
@@ -89,16 +112,19 @@
   #define SOURCES \
     storage_BR_sz.dna \
     storage_BR_town.dna \
-    storage_BR_english.dna \
-    storage_BR_japanese.dna \
     winter_storage_BR.dna \
-    halloween_props_storage_BR.dna \
+    halloween_props_storage_BR.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_BR_$[LANGUAGE].dna \
+    the_burrrgh_3100_$[LANGUAGE].dna the_burrrgh_3200_$[LANGUAGE].dna the_burrrgh_3300_$[LANGUAGE].dna \
+    the_burrrgh_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_BR_english.dna \
     the_burrrgh_3100_english.dna the_burrrgh_3200_english.dna the_burrrgh_3300_english.dna \
-    the_burrrgh_3100_castillian.dna the_burrrgh_3200_castillian.dna \
-    the_burrrgh_3100_japanese.dna the_burrrgh_3200_japanese.dna \
-    the_burrrgh_sz_english.dna \
-    the_burrrgh_sz_castillian.dna \
-    the_burrrgh_sz_japanese.dna
+    the_burrrgh_sz_english.dna
+  #endif
   #define PHASE 8
 #end install_dna
 
@@ -106,16 +132,19 @@
 #begin install_dna
   #define SOURCES \
     storage_DG_sz.dna storage_DG_town.dna \
-    storage_DG_english.dna \
-    storage_DG_japanese.dna \
     winter_storage_DG.dna \
-    halloween_props_storage_DG.dna \
+    halloween_props_storage_DG.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_DG_$[LANGUAGE].dna \
+    daisys_garden_5100_$[LANGUAGE].dna daisys_garden_5200_$[LANGUAGE].dna daisys_garden_5300_$[LANGUAGE].dna \
+    daisys_garden_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_DG_english.dna \
     daisys_garden_5100_english.dna daisys_garden_5200_english.dna daisys_garden_5300_english.dna \
-    daisys_garden_5100_castillian.dna daisys_garden_5200_castillian.dna daisys_garden_5300_castillian.dna \
-    daisys_garden_5100_japanese.dna daisys_garden_5200_japanese.dna daisys_garden_5300_japanese.dna \
-    daisys_garden_sz_english.dna \
-    daisys_garden_sz_castillian.dna \
-    daisys_garden_sz_japanese.dna
+    daisys_garden_sz_english.dna 
+  #endif
   #define PHASE 8
 #end install_dna
 
@@ -123,17 +152,21 @@
 #begin install_dna
   #define SOURCES \
     storage_DL_sz.dna storage_DL_town.dna \
-    storage_DL_english.dna \
-    storage_DL_japanese.dna \
     winter_storage_DL.dna \
-    halloween_props_storage_DL.dna \
+    halloween_props_storage_DL.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_DL_$[LANGUAGE].dna \
+    donalds_dreamland_9100_$[LANGUAGE].dna \
+    donalds_dreamland_9200_$[LANGUAGE].dna \
+    donalds_dreamland_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_DL_english.dna \
     donalds_dreamland_9100_english.dna \
-    donalds_dreamland_9100_castillian.dna \
-    donalds_dreamland_9100_japanese.dna \
     donalds_dreamland_9200_english.dna \
-    donalds_dreamland_sz_english.dna \
-    donalds_dreamland_sz_castillian.dna \
-    donalds_dreamland_sz_japanese.dna
+    donalds_dreamland_sz_english.dna
+  #endif
   #define PHASE 8
 #end install_dna
 
@@ -162,12 +195,19 @@
 #begin install_dna
   #define SOURCES \
     storage_GS_sz.dna \
-    storage_GS_english.dna \
-    goofy_speedway_sz_english.dna \
     halloween_props_storage_GS.dna \
     crashed_leaderboard_storage_GS.dna \
     urban_track_town.dna \
     urban_track_town_B.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_GS_$[LANGUAGE].dna \
+    goofy_speedway_sz_$[LANGUAGE].dna 
+  #else
+    #define SOURCES $[SOURCES] \
+    storage_GS_english.dna \
+    goofy_speedway_sz_english.dna
+  #endif
   #define PHASE 6
 #end install_dna
 
@@ -175,18 +215,32 @@
 #begin install_dna
   #define SOURCES \
     storage_OZ_sz.dna \
-    halloween_props_storage_OZ.dna \
+    halloween_props_storage_OZ.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_OZ_$[LANGUAGE].dna \
+    outdoor_zone_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
     storage_OZ_english.dna \
-    outdoor_zone_sz_english.dna 
+    outdoor_zone_sz_english.dna
+  #endif
   #define PHASE 6
 #end install_dna
 
 // Golf Zone 
 #begin install_dna
   #define SOURCES \
-    storage_GZ_sz.dna \
+    storage_GZ_sz.dna
+  #if $[LANGUAGE]
+    #define SOURCES $[SOURCES] \
+    storage_GZ_$[LANGUAGE].dna \
+    golf_zone_sz_$[LANGUAGE].dna
+  #else
+    #define SOURCES $[SOURCES] \
     storage_GZ_english.dna \
     golf_zone_sz_english.dna
+  #endif
   #define PHASE 6
 #end install_dna
 
