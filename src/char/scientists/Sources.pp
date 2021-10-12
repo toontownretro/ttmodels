40d7a265a443e9df1cf17eeb308ab79ab3bec94d
Bob <54867422+Bob-by@users.noreply.github.com>
2021-10-12 21:17:22 +0200

    Fixes

---
+++ src/char/scientists/Sources.pp
@@ -0,0 +1,30 @@
+// Placeholder fix until the rig uses them properly
+
+#define DIR_TYPE models
+#define INSTALL_TO models/char
+
+#begin install_egg
+  #define UNPAL_SOURCES_NC \
+    tt_a_chr_dgl_shorts_head_scientistGame.egg \
+	tt_a_chr_dgl_shorts_head_scientistJealous.egg \
+	tt_a_chr_dgl_shorts_head_scientistWork.egg \
+	tt_a_chr_dgl_shorts_legs_scientistGame.egg \
+	tt_a_chr_dgl_shorts_legs_scientistJealous.egg \
+	tt_a_chr_dgl_shorts_legs_scientistWork.egg \
+	tt_a_chr_dgl_shorts_torso_scientistGame.egg \
+	tt_a_chr_dgl_shorts_torso_scientistJealous.egg \
+	tt_a_chr_dgl_shorts_torso_scientistWork.egg \
+	tt_a_chr_dgm_shorts_head_scientistEmcee.egg \
+	tt_a_chr_dgm_shorts_legs_scientistEmcee.egg \
+	tt_a_chr_dgm_shorts_torso_scientistEmcee.egg \
+	tt_a_chr_dgs_shorts_head_scientistGame.egg \
+	tt_a_chr_dgs_shorts_head_scientistJealous.egg \
+	tt_a_chr_dgs_shorts_head_scientistWork.egg \
+	tt_a_chr_dgs_shorts_legs_scientistGame.egg \
+	tt_a_chr_dgs_shorts_legs_scientistJealous.egg \
+	tt_a_chr_dgs_shorts_legs_scientistWork.egg \
+	tt_a_chr_dgs_shorts_torso_scientistGame.egg \
+	tt_a_chr_dgs_shorts_torso_scientistJealous.egg \
+	tt_a_chr_dgs_shorts_torso_scientistWork.egg
+  #define PHASE 4
+#end install_egg
